734fc07261f59993f63f63559b4de7e2b6b4eca4
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-03-25 19:22:55 -0300

    created entity Student

--- StudentLambda/Entities/Student.cs
+++ StudentLambda/Entities/Student.cs
@@ -5,5 +5,14 @@ namespace StudentLambda.Entities
     [DynamoDBTable("students")]
     public class Student
     {
+        [DynamoDBHashKey("id")]
+        public int? Id { get; set; }
+
+        [DynamoDBProperty("first_name")]
+        public string FirstName { get; set; }
+        [DynamoDBProperty("last_name")]
+        public string LastName { get; set; }
+        [DynamoDBProperty("class")]
+        public int Class { get; set; }  
     }
 }
