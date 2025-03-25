a7467a9a5191c7cae75a1b5d0f3ace6fcc5e9d26
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-03-25 19:23:44 -0300

    created lambda functions for api-gateway with dynamoDB

--- StudentLambda/Function.cs
+++ StudentLambda/Function.cs
@@ -1,4 +1,9 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using StudentLambda.Entities;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -8,14 +13,41 @@ namespace StudentLambda;
 public class Function
 {
     
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input">The event for the Lambda function handler to process.</param>
-    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
-    /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public async Task<List<Student>> GetAllStudentsAsync(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
     {
-        return input.ToUpper();
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        DynamoDBContext dBContext = new DynamoDBContext(client);
+        var data = await dBContext.ScanAsync<Student>(default).GetRemainingAsync();
+        return data;
+    }
+
+    public async Task<Student> GetStudentByIdAsync(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+    {
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        DynamoDBContext dbContext = new DynamoDBContext(client);
+        string idFromPath = request.PathParameters["id"];
+        int id = Int32.Parse(idFromPath);
+
+        var student = await dbContext.LoadAsync<Student>(id);
+        if (student == null)
+            throw new Exception("Not Found!");
+        return student;
+    }
+
+    public async Task<APIGatewayHttpApiV2ProxyResponse> CreateStudentAsync(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+    {
+        var studentRequest = JsonConvert.DeserializeObject<Student>(request.Body);
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        DynamoDBContext dbContext =  new DynamoDBContext(client);
+
+        await dbContext.SaveAsync<Student>(studentRequest);
+        var message = $"Student with ID {studentRequest?.Id} Created";
+        LambdaLogger.Log(message);
+
+        return new APIGatewayHttpApiV2ProxyResponse
+        {
+            Body = message,
+            StatusCode = 200,
+        };
     }
 }
