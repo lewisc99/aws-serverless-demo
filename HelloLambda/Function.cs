7bbf3ef098071c15904d30e2372438e99fcde680
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-03-25 17:25:32 -0300

    created HelloLambda Function

--- HelloLambda/Function.cs
+++ HelloLambda/Function.cs
@@ -1,3 +1,4 @@
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -14,8 +15,15 @@ public class Function
     /// <param name="input">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public APIGatewayHttpApiV2ProxyResponse FunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
     {
-        return input.ToUpper();
+        request.QueryStringParameters.TryGetValue("name", out var name);
+        name = name ?? "John Doe";
+        var message = $"Hello {name}, from AWS lambda";
+        return new APIGatewayHttpApiV2ProxyResponse
+        {
+            Body = message,
+            StatusCode = 200,
+        };
     }
 }
