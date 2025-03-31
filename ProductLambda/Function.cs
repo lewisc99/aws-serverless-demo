708d33850c489985ec7d5d8dffd8fc7f326f2929
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-03-31 20:09:37 -0300

    Refactor FunctionHandler to use CreateProductRequest

--- ProductLambda/Function.cs
+++ ProductLambda/Function.cs
@@ -7,15 +7,30 @@ namespace ProductLambda;
 
 public class Function
 {
-    
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
-    /// <param name="input">The event for the Lambda function handler to process.</param>
-    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public CreateProductResponse FunctionHandler(CreateProductRequest input, ILambdaContext context)
     {
-        return input.ToUpper();
+        // Assume Product is Saved to DB
+        var response = new CreateProductResponse();
+        response.ProductId = Guid.NewGuid().ToString();
+        response.Name = input.Name;
+        response.Description = input.Description;
+        return response;
+    }
+    public class CreateProductRequest
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+    public class CreateProductResponse
+    {
+        public string? ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
     }
 }
